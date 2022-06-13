72e428b14a7d90912d9dc77eb64b975dace94a6f
Sinthia19 <sinthiaaudreyyy19.com>
2022-06-13 21:07:04 +0700

    List Categories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import { Row, Col, Container } from "react-bootstrap"
+import {Hasil, ListCategories, NavbarComponent, Footer, Menus} from './components'
+import React, { Component } from 'react'
+import axios from "axios";
 import './App.css';
+import { API_URL } from './utils/constants'
 
-function App() {
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+      categoryYangDipilih: 'Minuman'
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "products?category.nama="+this.state.categoryYangDipilih)
+      .then((res) => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+
+    changeCategory = (value) => {
+      this.setState({
+        categoryYangDipilih: value,
+        menus: []
+      })
+
+    axios
+    .get(API_URL + "products?category.nama="+value)
+    .then((res) => {
+      const menus = res.data;
+      this.setState({ menus });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+render() {
+  const { menus, categoriYangDipilih } = this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <NavbarComponent/>
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+          <ListCategories
+                changeCategory={this.changeCategory}
+                categoriYangDipilih={categoriYangDipilih}
+              />
+          <Col>
+          <h4><strong>Daftar Product</strong></h4>
+          <hr/>
+          <Row>
+            {menus && menus.map((menu) => (
+              <Menus
+              key={menu.id}
+              menu={menu}
+                 />
+            ))}
+          </Row>
+          </Col>
+          <Hasil/>
+          </Row>
+        </Container>
+        <Footer/>
+      </div>
+      </div>
   );
 }
-
-export default App;
+}
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Hasil from './Hasil'
+import ListCategories from './Listcategories'
+import NavbarComponent from './NavbarComponent'
+import Footer from './Footer'
+import Menus from './Menus'
+
+export { Hasil, ListCategories, NavbarComponent, Footer, Menus }
